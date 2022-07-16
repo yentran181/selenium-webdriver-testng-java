11b619775997fe851fdd3d1904639c7fb5e6007f
yentt <yentran18197@gmail.com>
2022-07-16 16:29:48 +0700

    Create new repository

---
+++ selenium/webdriver/Topic_01_Setup_Environment.java
@@ -0,0 +1,51 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_Setup_Environment {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://servertest.econtractapp.efy.com.vn/login");
+	}
+
+	@Test
+	public void TC_01_ValidateCurrentUrl() {
+		// Login Page Url matching
+		String loginPageUrl = driver.getCurrentUrl();
+		Assert.assertEquals(loginPageUrl, "http://servertest.econtractapp.efy.com.vn/login");
+	}
+
+	@Test
+	public void TC_02_ValidatePageTitle() {
+		// Login Page title
+		String loginPageTitle = driver.getTitle();
+		Assert.assertEquals(loginPageTitle, "Facebook – log in or sign up");
+	}
+
+	@Test
+	public void TC_03_LoginFormDisplayed() {
+		// Login form displayed
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
